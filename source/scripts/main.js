12f375fbeac88046fb13ce17c4d3555128c06f31
eunicornbread <40338933+eunicornbread@users.noreply.github.com>
2019-02-16 21:17:46 -0600

    Work on HTML CSS of the mp

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+window.onscroll = function() {
+	if (document.documentElement.scrollTop == 0) {
+		document.getElementById("navbar").classList.add("big");	
+	} else {
+		document.getElementById("navbar").classList.remove("big");
+	}
+
+
+};
