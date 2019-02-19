b942bf9690307ef92e33d23e9bb7f38e0a77112f
eunicornbread <40338933+eunicornbread@users.noreply.github.com>
2019-02-18 19:17:30 -0600

    Style and format code. Work on carousel.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,4 +1,6 @@
+/* Navbar */
 window.onload = window.onscroll = function() {
+	/* Navbar resizing */
 	var scrlamnt = document.documentElement.scrollTop;
 	if (scrlamnt == 0) {
 		document.getElementById("navbar").classList.add("big");	
@@ -6,8 +8,10 @@ window.onload = window.onscroll = function() {
 		document.getElementById("navbar").classList.remove("big");
 	}
 
-	console.log(scrlamnt);
+	/* For debugging purpose */
+	/* console.log(scrlamnt); */
 
+	/* Position indicator */
     if (scrlamnt > (document.getElementById("skills").offsetTop 
     					- document.getElementById("navbar").clientHeight)) {
 		document.getElementById("goto-skills").classList.add("active");
@@ -26,8 +30,10 @@ window.onload = window.onscroll = function() {
 	}
 };
 
+/* Modal */
 var modal1 = document.getElementById('modal1');
 var item1 = document.getElementById('item1');
+var close1 = document.getElementById('close1');
 
 item1.onclick = function() {
 	modal1.style.display = "block";
@@ -35,6 +41,7 @@ item1.onclick = function() {
 
 var modal2 = document.getElementById('modal2');
 var item2 = document.getElementById('item2');
+var close2 = document.getElementById('close2');
 
 item2.onclick = function() {
 	modal2.style.display = "block";
@@ -42,6 +49,7 @@ item2.onclick = function() {
 
 var modal3 = document.getElementById('modal3');
 var item3 = document.getElementById('item3');
+var close3 = document.getElementById('close3');
 
 item3.onclick = function() {
 	modal3.style.display = "block";
@@ -49,6 +57,7 @@ item3.onclick = function() {
 
 var modal4 = document.getElementById('modal4');
 var item4 = document.getElementById('item4');
+var close4 = document.getElementById('close4');
 
 item4.onclick = function() {
 	modal4.style.display = "block";
@@ -56,6 +65,7 @@ item4.onclick = function() {
 
 var modal5 = document.getElementById('modal5');
 var item5 = document.getElementById('item5');
+var close5 = document.getElementById('close5');
 
 item5.onclick = function() {
 	modal5.style.display = "block";
@@ -63,34 +73,65 @@ item5.onclick = function() {
 
 var modal6 = document.getElementById('modal6');
 var item6 = document.getElementById('item6');
+var close6 = document.getElementById('close6');
 
 item6.onclick = function() {
 	modal6.style.display = "block";
 }
 
 window.onclick = function(event) {
-	if (event.target == modal1) {
+	if (event.target == modal1 || event.target == close1) {
 		modal1.style.display = "none";
 	}
 
-	if (event.target == modal2) {
+	if (event.target == modal2 || event.target == close2) {
 		modal2.style.display = "none";
 	}
 
-	if (event.target == modal3) {
+	if (event.target == modal3 || event.target == close3) {
 		modal3.style.display = "none";
 	}
 
-	if (event.target == modal4) {
+	if (event.target == modal4 || event.target == close4) {
 		modal4.style.display = "none";
 	}
 
-	if (event.target == modal5) {
+	if (event.target == modal5 || event.target == close5) {
 		modal5.style.display = "none";
 	}
 
-	if (event.target == modal6) {
+	if (event.target == modal6 || event.target == close6) {
 		modal6.style.display = "none";
 	}
-	
+}
+
+/* Carousel */
+
+document.getElementById("left").onclick = function() {
+	moveLeft();
+	updateSlide();
+}
+document.getElementById("right").onclick = function() {
+	moveRight();
+	updateSlide();
+}
+
+var index = 0;
+updateSlide(index);
+
+function moveLeft() {
+	index = index + 1;
+}
+
+function moveRight() {
+	index = index - 1;
+}
+
+function updateSlide() {
+  var slides = document.getElementsByClassName("slide");
+  index = (index + slides.length) % slides.length;
+  for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[index].style.display = "block";
 }
